f3d0f71c33c46bbbb2a037a1dcda8f653eabe2f7
Silva01 <silva20102@gmail.com>
2016-12-03 21:45:14 -0200

    Criando projeto base

---
+++ app/app/scripts/controllers/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appApp
+ */
+  app.controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
